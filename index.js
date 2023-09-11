8ba5621be20e33f6feb694687b2a7b52d692751b
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-09-11 19:15:37 -0400

    added index.js onto repo , nearly finalized it more things needed

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const questions = [{ type: "input", name: "letters", message: "Choose 3 letters" }, { type: "input", name: "textColor", message: "Choose a color for the text" }, { type: "list", name: "shape", message: "Choose a shape", choices: ["Square", "Circle", "Triangle"] }, { type: "input", name: "shapeColor", message: "Choose a color for the shape" }];
+
+inquirer.prompt(questions).then(
+    response => {
+        if (response.shape == "Square") {
+            const svg = new Square(response.letters, response.textColor, response.shapeColor);
+            return svg.render();
+        }
+        else if (response.shape == "Circle") {
+            const svg = new Circle(response.letters, response.textColor, response.shapeColor);
+            return svg.render();
+        }
+        else if (response.shape == "Triangle") {
+            const svg = new Triangle(response.letters, response.textColor, response.shapeColor);
+            return svg.render();
+        }
+    }).then(response => {
+        createLogo('logo.svg', response);
+    });
+
+function createLogo(fileName, svg) {
+
+    fs.writeFile(fileName, svg, (error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(" SVG file generated ");
+        }
+    });
+}    
